7d6802fac7c38a00796b9ac8bc98f90cbce0e4b1
michal.gubanski <Michal.Gubanski@thomsonreuters.com>
2017-03-24 09:26:59 +0100

    searching and lodash

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,12 +6,19 @@ class SearchBar extends Component {
         super(props);
         this.state = { term: '' };
     }
+
     render() {
         return (<div className="search-bar">
             <input
                 value={this.state.term}
-                onChange={event => this.setState({ term: event.target.value })} />
-        </div>);
+                onChange={event => this.onInputChange(event.target.value)} />
+        </div>
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 
 const API_KEY = 'AIzaSyBJR-io9vsLryXV9BLIrxF9zEPOm2i0Xyw';
 
@@ -15,26 +16,28 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({ key: API_KEY, term: 'pizza' }, videos => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState(
         {
           videos: videos,
           selectedVideo: videos[0]
-        }
-      );
-    }
-
-    );
+        });
+    });
   }
 
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
-          onVideoSelect={selectedVideo => this.setState({ selectedVideo }) }
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
           videos={this.state.videos} />
       </div>
     )
