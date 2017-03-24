70e9dca81e3e47d722e4136cdcc6537583f9d288
michal.gubanski <Michal.Gubanski@thomsonreuters.com>
2017-03-24 13:18:49 +0100

    hotfix

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,11 +8,13 @@ class SearchBar extends Component {
     }
 
     render() {
-        return (<div className="search-bar">
-            <input
-                value={this.state.term}
-                onChange={event => this.onInputChange(event.target.value)} />
-        </div>
+        return (
+            <div className="search-bar">
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)}
+                />
+            </div>
         );
     }
 
